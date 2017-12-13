2bfccafa8fd3c637102149c28b25dcf075212a81
Wes <westonrwright@gmail.com>
2017-12-13 00:35:40 -0600

    removing home component not found error

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,8 @@ import { trigger,style,transition,animate,keyframes,query,stagger } from '@angul
             style({opacity: 0, transform: 'translateY(-75%)', offset:0}),
             style({opacity: .5, transform: 'translateY(35px)', offset:.3}),
             style({opacity: 1, transform: 'translateY(0)', offset:1})
-          ]))])), {optional: true})])
+          ]))]), {optional: true})
+        ])
     ])
   ]
 })
